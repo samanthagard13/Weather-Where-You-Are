1d18ea1e993640ee398bf0f345ca580c1d9c4d67
samantha gard <samantha.gard13@gmail.com>
2023-07-11 14:50:54 -0400

    js rework to make html dynamic

--- JS/script.js
+++ JS/script.js
@@ -1,27 +1,49 @@
 $(document).ready(function() {
 let now = dayjs();
-var APIKey = '0150c3b12a9d7824633c3bc8cc8ce43c';
-let requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={0150c3b12a9d7824633c3bc8cc8ce43c}'
+let apiKey = '0150c3b12a9d7824633c3bc8cc8ce43c';
+let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${apiKey}`;
+let buttonContainer = $('#button-container');
 let citiesArray = [
     { name: 'Atlanta', lat: 33.7490, lon: -84.3880 }, 
+    { name: 'Denver', lat: 39.7392, lon: 104.9903 },
+    { name: 'Seattle', lat: 47.6062, lon: 122.3321 },
 ];
-let Btns = [
-    $('#atlantaBtn'), 
-];
+let currentCity = $('#currentcity');
+let currentTemp = $('#currenttemp');
+let currentWind = $('#currentwind');
+let currentHumidity = $('#currenthumidity');
 
-Btns.forEach(btn => {
-    btn.on('click', function() {
-        let city = citiesArray.find(city => city.name);
-        let lat = city.lat;
-        let lon = city.lon;
-        let updatedRequestUrl = requestUrl.replace('{lat}', lat).replace('{lon}', lon);
-        fetch(requestUrl)
+var displayCity = function() {
+    var lat = $(this).data('lat');
+    var lon = $(this).data('lon');
+    var url = requestUrl.replace('{lat}', lat).replace('{lon}', lon);
+
+    fetch(url).then(function(response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                currentCityBox(data)
+            });
+        } else {
+            alert('Error: ' + response.statusText);
+        }
     });
-    fetch(updatedRequestUrl)
-        .then(response => response.json())
-        .then(data => {
-        console.log(data);
-})
+};
+
+for (var i = 0; i < citiesArray.length; i++) {
+    var btnName = citiesArray[i].name;
+    var cityBtns = $('<button>');
+    cityBtns.addClass('h4 p-2');
+    cityBtns.text(btnName);
+    cityBtns.data('lat', citiesArray[i].lat);
+    cityBtns.data('lon', citiesArray[i].lon);
+    buttonContainer.append(cityBtns);
+    cityBtns.on('click', displayCity);
+};
+
+var currentCityBox = function(event) {
+    var cityName = event.target.textContent;
+    currentCity.text(cityName);
+};
 
 function updateCurrentTime() {
 $('#today').text(now.format('dddd, MMMM D YYYY'));
@@ -31,12 +53,9 @@ updateCurrentTime();
 
 setInterval(updateCurrentTime(), 1000);
 
-});
-});
-    
-// take in search input
-//get url info
-//display in current city box
+displayCity();
 
-//current city get info for 5 days
-//display each day on a card with weather icon
+});
+
+
+
